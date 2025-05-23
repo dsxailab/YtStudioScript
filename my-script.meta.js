a5b981dc5bafc42d00f321c1aa833b24c3b47009
dsxailab <dsxailab@users.noreply.github.com>
2025-05-23 14:50:25 +0000

    deploy: dsxailab/novelutils@614479af6e28230711e270688e6f34b744991b0c

--- my-script.meta.js
+++ my-script.meta.js
@@ -188,12 +188,13 @@
                 if (endNum > lastVidInput.value) {
                     endNum = lastVidInput.value;
                 }
-                searchBox.value = `${nextNumber} ${endNum}`;
+                const filter = `${nextNumber} ${endNum}`;
+                searchBox.value = filter;
                 searchBox.dispatchEvent(new InputEvent("input", { bubbles: true }));
                 await sleep(2000);
                 const vids = document.getElementsByClassName("ytcp-entity-card title");
                 for (let v of vids) {
-                    if (v.textContent.trim() == searchBox.value) {
+                    if (v.textContent.trim() == filter) {
                         v.click();
                         break;
                     }
