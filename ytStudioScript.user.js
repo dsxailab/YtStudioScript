16137bcad3a14366b1b9b8fef6bbc24d70facaf2
dsxailab <dsxailab@users.noreply.github.com>
2025-05-25 13:48:25 +0000

    deploy: dsxailab/novelutils@10f31ee13a1c0029368c77f95eb73562755b4826

--- ytStudioScript.user.js
+++ ytStudioScript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ytStudio
 // @namespace    http://tampermonkey.net/
-// @version      2025.5.25.2
+// @version      2025.5.25.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://studio.youtube.com/*
